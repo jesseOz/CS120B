5bd11531d18c20beac7dcad8553f823a84aa920d
Jesse Cazun <jcazu001@wch136-22.cs.ucr.edu>
2021-10-03 18:50:06 -0700

    Completed part 3

--- source/jcazu001_lab2_part1.c
+++ source/jcazu001_lab2_part1.c
@@ -1,7 +1,7 @@
 /*	Author: jcazu001
  *  Partner(s) Name: 
  *	Lab Section: 22
- *	Assignment: Lab 2  Exercise 1
+ *	Assignment: Lab 2  Exercise 3
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -15,24 +15,37 @@
 
 int main(void) {
 	DDRA = 0x00;// PORTA = 0xFF; // Configure port A's 8 pins as inputs
-	DDRB = 0xFF;// PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
-//	unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
+	DDRC = 0xFF;
+//	unsigned chcr tmpB = 0x00; // Temporary variable to hold the value of B
 //	unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
 while(1) {
-if ((PINA1 == 0) && (PINA0 == 1))
+
+if (PORTA == 0x00)
 {
-PINB0  = 1; 
+PORTC = 0x04;
 }
-else
+
+if (PORTA == 0x01 || PORTA = 0x02 || PORTA = 0x03 || PORTA = 0x04)
 {
-PINB 0 = 0;
+PORTC = 0x03; 
 }
 
+if (PORTA == 0x03 || PORTA == 0x05 || PORTA == 0x09 || PORTA == 0x0A || PORTA == 0x06 || PORTC == 0x0C)
+{
+PORTC = 0x02;
 }
 
+if (PORTA == 0x07 || PORTA == 0x0E || PORTA == 0x0B || PORTA == 0x0D)
+{
+PORTC = 0x01;
+}
 
-														 												 					return 0;
-		 																	 					
+if (PORTA == 0x0F)
+{
+PORTC == 0x80;
+}
+}
+														 												 					return 0;		 																	 					
 }
 				 											
 
